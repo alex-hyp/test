15116b34e24dfc2fea0a2762ce43577575514518
alex-hyp <alex.hyp.17@gmail.com>
2025-10-27 15:23:48 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+
+void print_vect(vector<int> &vin)
+{
+
+    for (auto i : vin)
+    {
+
+        cout << i << endl;
+    }
+    cout << endl;
+}
+
+int main()
+{
+
+    vector<int> V = {1, 2, 3, 5, 10};
+    print_vect(V);
+
+    int x = 1;
+    cout << endl;
+
+    while (x)
+    {
+        cout << "add: ";
+        cin >> x;
+
+        if (x)
+        {
+            V.push_back(x);
+            sort(V.begin(), V.end());
+            print_vect(V);
+        }
+    }
+
+    system("pause");
+
+    return 0;
+}
+
